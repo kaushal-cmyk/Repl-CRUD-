705f7709d81de526dc5483d52cdb37f3c4d44adf
Kaushal Adhikari <kaushaladhikari098@gmail.com>
2024-08-13 16:30:21 +0545

    main

--- obj/Debug/net6.0/Student.AssemblyInfo.cs
+++ obj/Debug/net6.0/Student.AssemblyInfo.cs
@@ -14,7 +14,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Student")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d9b91fbb17d8934ec92b3b724108cc5be8abdb1")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+>>>>>>> parent of 3dd9acf (completed)
 [assembly: System.Reflection.AssemblyProductAttribute("Student")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Student")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
